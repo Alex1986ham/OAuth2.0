d7b002e0f63aeb4132f998eb69eecdf9b2f6043c
Irina K <Irina@Irinas-Air.fritz.box>
2018-05-17 20:48:01 +0200

    2nd commit test

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+# just a test
+
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
