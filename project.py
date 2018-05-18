3a180e7d52ecdb5a6b1a4ddf576d008d4f7925d1
Irina K <Irina@Irinas-Air.fritz.box>
2018-05-18 18:33:52 +0200

    Back to a working version

--- project.py
+++ project.py
@@ -1,5 +1,3 @@
-# just a test
-
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
